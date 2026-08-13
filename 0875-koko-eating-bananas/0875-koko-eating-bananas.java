94f67e3dbf7167061ba04af087e910f0baf93ce9
Arun Kumar <130972134+arunkumar93929@users.noreply.github.com>
2026-08-13 13:59:12 +0530

    Time: 15 ms (36.08%), Space: 48.1 MB (32.92%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,25 @@
+//The following codes pass all cases
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1, right =Arrays.stream(piles).max().getAsInt();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (calcuhrs(piles, mid) <= h) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+    
+    private int calcuhrs(int[] piles, int speed) {
+        int hours = 0;
+        for (int i = 0; i < piles.length; i++) {
+            hours += piles[i] / speed;
+            if (piles[i] % speed > 0) hours++;
+        }
+        return hours;
+    }
+    
+}
